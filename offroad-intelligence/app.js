0435b630296c413e185d7df8f65e7b43a743c54a
Tezcanli-a <aysegul.tezcanli@grammer.com>
2026-08-19 14:19:45 +0200

    Update app.js

--- offroad-intelligence/app.js
+++ offroad-intelligence/app.js
@@ -1973,6 +1973,7 @@ function renderOutcomeEngine(){
     `).join('');
 }
 function renderExecutiveAgenda() {
+  console.log(state.data.assessments);
 
   const assessments = state.data.assessments || [];
 
